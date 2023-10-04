4c46f42b12af6f8d9584244d496c2bcc1da6caa1
Dannyel <dannyel_alulema_2001@hotmail.com>
2023-10-03 20:53:03 -0500

    fix(app): Corrección de errores en el API y en el marco de trabajo

--- app/middlewares/AdminMiddleware.php
+++ app/middlewares/AdminMiddleware.php
@@ -8,16 +8,12 @@ class AdminMiddleware
     use Responser;
 
     public function handle() {
-        if (!isset($_REQUEST['decoded_jwt'])) {
+        if (!isset($_REQUEST['decoded_jwt']))
             $this->errorResponse('Not Found', 404);
-            die();
-        }
 
         $user = $_REQUEST['decoded_jwt'];
 
-        if ($user->role != 'ADMIN') {
+        if ($user->role != 'ADMIN')
             $this->errorResponse('Not Found', 404);
-            die();
-        }
     }
 }
